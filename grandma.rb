eb92b4ce28402236cdc2751666016ff9a88a824a
elsa chen <snowing222@gmail.com>
2020-05-15 12:55:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,11 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if phrase=="I LOVE YOU GRADMA!"
+if phrase==phrase.upcase
+  
   "I LOVE YOU TOO PUMPKIN!"
 
-elsif phrase=phrase.upcase
+elsif phrase==phrase.upcase
   "NO, NOT SINCE 1938!"
 
 else
